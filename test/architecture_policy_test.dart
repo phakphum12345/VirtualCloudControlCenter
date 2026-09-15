aa63756d3dc4a2e56e78e45347dfa503d81771b7
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-15 15:22:41 +0700

    Delete test/architecture_policy_test.dart

--- test/architecture_policy_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('architecture policy rules are respected', () {
-    // เพิ่มเงื่อนไขหรือการตรวจสอบสถาปัตยกรรมของคุณที่นี่
-    expect(true, isTrue);
-  });
-}
