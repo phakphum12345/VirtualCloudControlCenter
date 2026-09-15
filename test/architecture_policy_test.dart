333afc5d6ffdc8e750b68ef1f86e09a8630d8f76
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-15 14:20:40 +0700

    Update architecture_policy_test.dart

--- test/architecture_policy_test.dart
+++ test/architecture_policy_test.dart
@@ -1,1 +1,8 @@
+import 'package:test/test.dart';
 
+void main() {
+  test('architecture policy rules are respected', () {
+    // TODO: Add your architecture policy assertions here
+    expect(true, isTrue);
+  });
+}
